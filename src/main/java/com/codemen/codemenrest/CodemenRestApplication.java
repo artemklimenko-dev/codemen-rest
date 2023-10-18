63dc0e0ec8396c18010c91ffc97bdcbcbc8b841b
Artem Klimenko <artemklimenko.dev@gmail.com>
2023-10-18 21:35:11 +0300

    WIP: started project with spring boot and jwt

---
+++ src/main/java/com/codemen/codemenrest/CodemenRestApplication.java
@@ -0,0 +1,13 @@
+package com.codemen.codemenrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodemenRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodemenRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codemen/codemenrest/CodemenRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codemen.codemenrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodemenRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
